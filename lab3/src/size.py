fc4da42e8301bc6b3d76d7b467deb667139355b5
totoro <953834564@qq.com>
2021-02-16 15:59:25 +0800

    lab3: size of M

---
+++ lab3/src/size.py
@@ -0,0 +1,20 @@
+import os
+import csv
+from config import conf
+
+os.chdir(conf["WORKPATH"])
+dataset_path = conf['dataset_path']
+trainData = conf['trainData']
+with open(os.path.join(dataset_path, trainData), 'r', encoding='utf-8') as csvfile:
+    cs = list(csv.reader(csvfile))
+userId = set()
+movieId = set()
+for record in cs:
+    if int(record[0]) not in userId:
+        userId.add(int(record[0]))
+    if int(record[1]) not in movieId:
+        movieId.add(int(record[1]))
+# print(userId, len(userId))
+print(len(userId)) # 2173
+# print(movieId, len(movieId))
+print(len(movieId)) # 58431
